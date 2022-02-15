1749a9696a7e791ca2cc5998f13d783c8d2ce607
dflehel <44527037+dflehel@users.noreply.github.com>
2022-02-15 15:46:42 +0100

    Initial Commit

---
+++ gesturedetection WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  gesturedetection WatchKit Extension
+//
+//  Created by Lehel Dénes-Fazakas on 2022. 02. 15..
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ gesturedetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gesturedetection
+//
+//  Created by Lehel Dénes-Fazakas on 2022. 02. 15..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
